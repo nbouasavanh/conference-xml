fdf586a7a853f3bca43e895f6b36e1d1adb16dc9
Nim Bouasavanh <minxvnim@Nims-MacBook-Pro.local>
2023-08-17 10:40:17 -0500

    Initial commit

---
+++ src/main/java/com/pluralsight/model/Speaker.java
@@ -0,0 +1,25 @@
+package com.pluralsight.model;
+
+public class Speaker {
+
+    private String firstName;
+    private String lastName;
+
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+
+    }
+}
